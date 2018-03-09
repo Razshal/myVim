32c697505275ef1ea1a2ed3bb642ec716f03f9db
Razshal <fontenille.mathieu@gmail.com>
2018-03-09 12:37:21 +0000

    working script

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
-cp -aR .* $HOME
-git -C $HOME/.vim/bundle pull
+rm -f $HOME/.zshrc && cp .zshrc $HOME/
+rm -f $HOME/.zshrc && cp .vimrc $HOME/
+rm -rf $HOME/.vim && cp -R .vim $HOME/
+git clone https://github.com/flazz/vim-colorschemes.git $HOME/.vim/bundle/colorschemes
+git clone https://github.com/myusuf3/numbers.vim.git $HOME/.vim/bundle/numbers
+git clone https://github.com/vim-syntastic/syntastic.git $HOME/.vim/bundle/syntastic
