678934178a48db7bc76406362492119efe154c74
Razshal <fontenille.mathieu@gmail.com>
2018-09-27 15:41:08 +0200

    Added xbindkeys/xdotool config for anywhere mx2 mouse buttons + updated scripts and zshrc

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
-rm -f $HOME/.zshrc
-cp .zshrc $HOME/
-rm -f $HOME/.vimrc
-cp .vimrc $HOME/
-rm -rf $HOME/.vim
-cp -R .vim $HOME/
+rm -rf $HOME/.zshrc $HOME/.vimrc $HOME/.vim
+cp -R {.xbindkeysrc,.zshrc,.vimrc,.vim} $HOME
 git clone https://github.com/flazz/vim-colorschemes.git $HOME/.vim/bundle/colorschemes
 git clone https://github.com/myusuf3/numbers.vim.git $HOME/.vim/bundle/numbers
 git clone https://github.com/vim-syntastic/syntastic.git $HOME/.vim/bundle/syntastic
