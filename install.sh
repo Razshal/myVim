125ef40f2699283091e1f99b9165685c611a7c72
Razshal <fontenille.mathieu@gmail.com>
2019-02-15 16:14:02 +0100

    default git push behaviourwq

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 rm -rf $HOME/.zshrc $HOME/.vimrc $HOME/.vim
 cp -R {.xbindkeysrc,.zshrc,.vimrc,.vim} $HOME
 git config --global core.editor "vim"
+git config --global push.default
 git clone https://github.com/flazz/vim-colorschemes.git $HOME/.vim/bundle/colorschemes
 git clone https://github.com/myusuf3/numbers.vim.git $HOME/.vim/bundle/numbers
 git clone https://github.com/vim-syntastic/syntastic.git $HOME/.vim/bundle/syntastic
